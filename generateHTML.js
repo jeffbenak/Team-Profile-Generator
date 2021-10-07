f684aa6594dee8ed1caf411694011a425e0ccab3
Jeff Benak <jeff.benak@yahoo.com>
2021-10-07 18:55:59 -0500

    Updated generatehtml.js, also added a function that generates the html

--- generateHTML.js
+++ generateHTML.js
@@ -1,21 +1,38 @@
-function generateHtml(employees) {
-    let employeeCards = ``;
-    for (i = 0; i < employees.length; i++) {
-        
-    }
+function generateHtml(data) {
+  switch (data.choice) {
+    case 'Manager':
+      return `
+                <h3 id="name">${data.name}</h3>
+                <h4> ${data.choice}</h4>
+                <ul class="list">
+                  <li class="list-group">ID: ${data.id}</li>
+                  <li class="list-group">Email: <a href="mailto:${data.email}">${data.email}</a></li>
+                  <li class="list-group">Office Number: ${data.officeNumber}</li>
+                </ul>
+    `;
+    case 'Engineer':
+      return `
+                <h3 id="name">${data.name}</h3>
+                <h4>${data.choice}</h4>
+                <ul class="list">
+                  <li class="list-group">ID: ${data.id}</li>
+                  <li class="list-group">Email: <a href="mailto:${data.email}">${data.email}</a></li>
+                  <li class="list-group">GitHub: <a href="http://github.com/${data.github}/">${data.github}</a></li>
+                </ul>
+      `
+    case 'Intern':
+      return `
+                <h3 id="name">${data.name}</h3>
+                <h4><i class="fas fa-user-graduate"></i> ${data.choice}</h4>
+                <ul class="list">
+                  <li class="list-group">ID: ${data.id}</li>
+                  <li class="list-group">Email: <a href="mailto:${data.email}">${data.email}</a></li>
+                  <li class="list-group">School: ${data.school}</li>
+                </ul>
+      `
+  
+}}
 
-    let html = `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-  ${employeeCards}  
-</body>
-</html>`;
-return html;
 
-}
+
+module.exports = generateHtml;
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+const generateHtml = require('./generateHTML');
+
 
 
 const employees = [];
@@ -9,8 +12,8 @@ const employees = [];
 const mainQuestion = [
     {
         type: 'list',
-        message: 'What do you want to do?',
-        choices: ['Intern', 'Engineer', 'Quit'],
+        message: 'What would you like to add?',
+        choices: ['Intern', 'Engineer', 'Create Team'],
         name: 'choice',
     },
 ];
@@ -36,25 +39,6 @@ const managerQuestions = [
         type: 'input',
         message: 'What is your office number?',
         name: 'number',
-    },
-];
-
-const employeeQuestions = [
-    {
-        type: 'input',
-        message: 'What is your name?',
-        name: 'name',
-    },
-    {
-        type: 'input',
-        message: 'What is your employee Id?',
-        name: 'id',
-
-    },
-    {
-        type: 'input',
-        message: 'What is your email?',
-        name: 'email',
     },
 ];
 
@@ -105,16 +89,23 @@ const internQuestions = [
         name: 'school',
     },
 ];
+// Function that asks the user for manager information immediately when node index.js is ran
 
-function init(){
+function init() {
 inquirer.prompt(managerQuestions).then(function(answers){
-    console.log(answers);
+    const manager = new Manager(answers.name, answers.id, answers.email, answers.number)
+    employees.push(manager);
+    continuePrompt();
+
+    })
     
-})
+
 }
+
+// Function that continues the prompt after the init function is invoked
+
 function continuePrompt(){
 inquirer.prompt(mainQuestion).then(function(answers){
-    console.log(answers);
     if (answers.choice === 'Intern') {
         internPrompt();
     }
@@ -122,23 +113,69 @@ inquirer.prompt(mainQuestion).then(function(answers){
         engineerPrompt();
     }
     else if (answers.choice === 'Quit') {
-        
+        createTeam();
     }
-})
+});
 }
+
+// Function that asks the user certain questions for an engineer
+
 function engineerPrompt(){
 inquirer.prompt(engineerQuestions).then(function(answers){
-    console.log(answers);
+    let engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+    employees.push(engineer);
+    continuePrompt();
     
 })
 
 }
+
+// Function that asks the user certain questions for an intern
+
 function internPrompt(){
 inquirer.prompt(internQuestions).then(function(answers){
-    console.log(answers);
+    let intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+    employees.push(intern);
+    continuePrompt();
     
 })
+
+ //Function that will generate the app
 }
-function quitPrompt() {
+//  function quitPrompt() {
+//      let data = generateHtml(employees);
+//      fs.writeFile('index.html', data, function(err){
+//          if(err)throw err
+//          console.log('file written');
+//      })
     
+//  }
+// Function call to initialize app
+init();
+
+
+let html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
+</head>
+<body> 
+</body>
+</html>`;
+
+
+function createTeam(){
+
+for (i = 0; i < employees.length; i++) {
+    const employeeCard = generateHtml(employees[i]);
+    console.log(employeeCard);
+    html += employeeCard;
+}
+fs.writeFile('index.html', html, function(err){
+    if(err)throw err
+    console.log('file written');
+})
 }
